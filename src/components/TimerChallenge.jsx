3f0a5556695d6311eb9bd2c138f1cb0a5249ce37
Constant Vlad <vladok21381@gmail.com>
2025-11-17 21:02:12 +0100

    Added a modal window for result the game

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,8 +1,10 @@
 import { useState, useRef } from "react"
+import ResultModale from "./ResultModal.jsx";
 
 export default function TimerChallenge({title, targetTime}) {
 
   const timer = useRef();
+  const dialog = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timeExpired, setTimerExpired] = useState(false);
 
@@ -10,6 +12,7 @@ export default function TimerChallenge({title, targetTime}) {
     setTimerStarted(true);
     timer.current = setTimeout( () => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
   }
 
@@ -18,19 +21,22 @@ export default function TimerChallenge({title, targetTime}) {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timeExpired && <p>You lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined} >
-       {timerStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>)
+    <>
+      <ResultModale ref={dialog} targetTime={targetTime} result="lost"/>
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined} >
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
+    )
 }
